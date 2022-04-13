51543bb5897c312a8e1cd3f5c9b9d79016240952
marvbo <marvinbogenschuetz@gmx.de>
2022-04-13 10:45:52 +0200

    finished Aufgabe_3c

--- Uebung2/Aufgabe_03c.py
+++ Uebung2/Aufgabe_03c.py
@@ -9,13 +9,19 @@ extend extends a list by appending elements
 list1.append([1,2,3])
 list2.extend([1,2,3])
 
-print(list1, list2, sep='\n')
 list1.remove([1,2,3])
-print(list1)
 
 rlist = list1
 clist = rlist.copy()
 clist.insert(0,-3)
 clist.insert(0,-2)
 clist.insert(0,-3)
-print(clist)
+
+# remove 0 from clist
+clist.pop(clist.index(0))
+
+print(list1, list2, rlist, clist, sep= '\n')
+
+# by clearing original list the copy will be cleared too
+list1.clear()
+print(list1, list2, rlist, clist, sep= '\n')
