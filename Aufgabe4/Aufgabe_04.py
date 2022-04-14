98c540a301f0563488ddf313ce15fc8ccc974e1b
marvbo <marvinbogenschuetz@gmx.de>
2022-04-14 13:40:47 +0200

    update Aufgabe_04
    noch nicht fertig

--- Aufgabe4/Aufgabe_04.py
+++ Aufgabe4/Aufgabe_04.py
@@ -1,5 +1,23 @@
+import random
+import math
 import numpy as np
 
-matrix = []
-matrixgroese = input("Please enter the size of your NxN matrix: ")
+list = []
+eingabe = input("Please enter the size of your NxN matrix: ")
+eingabe = int(eingabe)
+matrixgroese = eingabe ** 2
+
+i = 0
+# build random values between -9 und 9 and put it to list
+while i < matrixgroese:
+    list.append(random.randint(-9, 9))
+    i += 1
+
+# list to array
+array = np.array(list)
+
+# array to matrix
+matrix = np.reshape(array, (eingabe,eingabe))
+print(matrix)
+
 
