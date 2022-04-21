f01e36a481081d3d295511dd1163021f23cc1dd5
marvbo <marvinbogenschuetz@gmx.de>
2022-04-21 15:10:37 +0200

    update KugelFunktionen
    added function oberfläche()
    
    evtl nochmal schauen ob Funktionalität so passt

--- KugelFunktionen/KugelFunktionen.py
+++ KugelFunktionen/KugelFunktionen.py
@@ -1,10 +1,10 @@
 import math
 def schnittRadius(radius = 1, winkel = 90):
-    a = radius*math.sin(math.radians(abs(winkel)%180))
+    a = radius*math.sin(math.radians(abs(winkel)))
     return a
 
 def schnittHoehe(radius = 1, winkel = 90):
-    h = radius*(1-math.cos(math.radians(abs(winkel%180))))
+    h = radius*(1-math.cos(math.radians(abs(winkel))))
     return h
 
 def schnittFlaeche(radius = 1, winkel = 90):
@@ -15,6 +15,10 @@ def teilVolumen(radius = 1, winkel = 90):
     h = schnittHoehe(radius, winkel)
     return math.pi/3*math.pow(h, 2)*(3*radius-h)
 
-print(schnittRadius())
-print(schnittFlaeche())
-print(schnittHoehe())
+def oberfläche(radius = 1, winkel = 90):
+    h = schnittHoehe(radius, winkel)
+    a = schnittRadius(radius, winkel)
+    return math.pi*(2*radius*h + pow(a, 2))
+
+print(oberfläche(1, 180))
+print(teilVolumen())
