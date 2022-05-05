78cd83267347d35f88c6ce9dd755ac8e14c761c9
Meschr <marcel-escher@gmx.de>
2022-05-05 12:00:15 +0200

    added functionality to complex class, added main script

--- Uebung4/Aufgabe_01.py
+++ Uebung4/Aufgabe_01.py
@@ -1,7 +1,21 @@
 class Complex:
+    real = 0.0
+    imag = 0.0
+    imagChar = 'i'
 
-    def __init__(self, real=0, imag=0):
-        self.__real = real
-        self.__imag = imag
-    
+    def __init__(self):
+        self.real = 0.0
+        self.imag = 0.0
 
+    def __init__(self, realPart, imagPart):
+        self.real = realPart
+        self.imag = imagPart
+
+    def __init__(self, realPart, imagPart, imagChar):
+        self.real = realPart
+        self.imag = imagPart
+
+    def disp(self):
+        complexString = "{0:f}{1:+f}{2}".format(self.real,self.imag,self.imagChar)
+        print(complexString)
+        return
---
+++ Uebung4/main.py
@@ -0,0 +1,4 @@
+import Aufgabe_01 as C
+
+c1 = C.Complex(-0.5,-0.3445)
+c1.disp()
