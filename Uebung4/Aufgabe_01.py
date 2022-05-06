7627635d1f53a4926f7a5ed374185e70e5f33e1d
marvbo <marvinbogenschuetz@gmx.de>
2022-05-06 13:44:56 +0200

    added function add()
    allows to add two complex numbers

--- Uebung4/Aufgabe_01.py
+++ Uebung4/Aufgabe_01.py
@@ -11,11 +11,20 @@ class Complex:
         self.real = realPart
         self.imag = imagPart
 
-    def __init__(self, realPart, imagPart, imagChar):
+    def __init__(self, realPart, imagPart, imagChar = 'i'):
         self.real = realPart
         self.imag = imagPart
 
+        if imagChar == 'i' or imagChar == 'j':
+            self.imagChar = imagChar
+        else:
+            self.imagChar = 'i'
     def disp(self):
         complexString = "{0:f}{1:+f}{2}".format(self.real,self.imag,self.imagChar)
         print(complexString)
         return
+
+
+def add(number1,number2):
+    return Complex(number1.real + number2.real, number1.imag + number2.imag, 'i')
+
--- Uebung4/main.py
+++ Uebung4/main.py
@@ -1,4 +1,9 @@
 import Aufgabe_01 as C
 
-c1 = C.Complex(-0.5,-0.3445)
+c1 = C.Complex(int(input("Enter real part of number")), int(input("Enter imaginary part of number")))
+c2 = C.Complex(int(input("Enter real part of number")), int(input("Enter imaginary part of number")))
+
 c1.disp()
+c2.disp()
+c3 = C.add(c1,c2)
+c3.disp()
